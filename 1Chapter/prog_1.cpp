819299f5a87dfd604a1a62ad4df9db4ecfcddbd1
Dominique Ingoglia <Dominique.Ingoglia@gmail.com>
2014-09-05 18:25:55 -0600

    prog_1

---
+++ 1Chapter/prog_1.cpp
@@ -0,0 +1,36 @@
+//Dominique Ingoglia
+//09/05/14
+//Programming assignment 1
+
+
+//include statement (s)
+#include <iostream>
+
+//using namespace statement
+using namespace std;
+
+//outputs the desired information and prompts the user to press a key to exit the program.
+int main()
+{
+//variable declaration
+  int num1;
+  int num2;
+  int num3;
+  int average;
+//assign value to variables
+ num1 = 125;
+ num2 = 28;
+ num3 = -25;
+ average= (num1+num2+num3)/3;
+
+//executable statements
+ cout << "num1= " << num1 << endl
+      << "num2= " << num2 << endl
+      << "num3= " << num3 << endl
+      << "average= " << average << endl	
+      << "Press any key to continue . . .";
+   cin.get(); 
+
+//return statement
+ return 0;
+}
