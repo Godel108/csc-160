9235c9447129de28423cbdb060587b28731a1e53
root <root@Universe.(none)>
2014-09-10 11:10:12 -0600

    chapt2

---
+++ 2chapter/hw2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+char letter = ' ';
+
+using namespace std;
+ int num1= 36 / 5;
+ int num2= 18 - 32 / 6 * 3;
+string message = 'first test';
+const int num3= 80 % 11;
+const int num4= 6 - 8 % 11;
+const double num5= 22.0 / 5;
+const double num6= 27 - 12 / 8.0;
+const int num7= 25 - 7 % 3 + 8 / 3;
+const double num8= 18.0 + 5.0 * 3.0 / 4.0;
+
+int main()
+{ 
+
+    cout << num1  << "\n" <<  num2   << "\n" <<  num3   << "\n" << num4    << "\n"<< num5  << "\n" << num6  << "\n" <<num7  << "\n" <<num8 << endl;
+      return 0;
+      } 
